49250840d94e64a32c720c494e966e022af378c9
Zahir <zahir.inspireme@gmail.com>
2021-01-18 03:09:43 +0800

    Create gLiveView_install.sh

---
+++ installation/gLiveView_install.sh
@@ -0,0 +1,4 @@
+sudo apt install bc tcptraceroute -y
+curl -s -o gLiveView.sh https://raw.githubusercontent.com/cardano-community/guild-operators/master/scripts/cnode-helper-scripts/gLiveView.sh
+curl -s -o env https://raw.githubusercontent.com/cardano-community/guild-operators/master/scripts/cnode-helper-scripts/env
+chmod 755 gLiveView.sh
