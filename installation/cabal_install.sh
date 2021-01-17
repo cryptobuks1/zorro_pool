987167c0653cc478dbe2280644a726525a37f16b
Zahir <zahir.inspireme@gmail.com>
2021-01-18 03:01:25 +0800

    Create cabal_install.sh

---
+++ installation/cabal_install.sh
@@ -0,0 +1,12 @@
+wget https://downloads.haskell.org/~cabal/cabal-install-3.2.0.0/cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz
+tar -xf cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz
+rm cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz cabal.sig
+mkdir -p ~/.local/bin
+mv cabal ~/.local/bin/
+echo "export PATH=~/.local/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+echo $PATH
+source .bashrc
+echo $PATH
+cabal update
+cabal --version
