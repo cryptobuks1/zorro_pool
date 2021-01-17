f92e9fc65ac0e44ee3a4ee64eb883a64505d37fd
Zahir <zahir.inspireme@gmail.com>
2021-01-18 03:07:10 +0800

    Create node.sh

---
+++ scripts/node.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/home/cardano/.local/bin/cardano-node run \
+  --database-path  ~/cnode/db/ \
+  --socket-path ~/cnode/sockets/node.socket \
+  --port 3000 \
+  --config ~/cnode/config/mainnet-config.json \
+  --topology ~/cnode/config/mainnet-topology.json
