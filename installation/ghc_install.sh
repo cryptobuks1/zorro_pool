32579d9600a0f19392a6bad5eae9a6bcdd5f5d8f
Zahir <zahir.inspireme@gmail.com>
2021-01-18 03:01:59 +0800

    Create ghc_install.sh

---
+++ installation/ghc_install.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/git
+cd ~/git
+
+wget https://downloads.haskell.org/ghc/8.10.2/ghc-8.10.2-x86_64-deb9-linux.tar.xz
+tar -xf ghc-8.10.2-x86_64-deb9-linux.tar.xz
+rm ghc-8.10.2-x86_64-deb9-linux.tar.xz
+cd ghc-8.10.2
+./configure
+sudo make install
+ghc --version
